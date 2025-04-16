61910a3841bffd93fc244bfa2100c440942a9562
Fatemah01 <mybackuppersonal01@gmail.com>
2025-04-16 14:10:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+# import streamlit as st
+# import pandas as pd
+# import joblib
+# from utils import preprocessor
+
+# def run():
+#     model = joblib.load(open('model.joblib', 'rb'))
+
+#     st.title("Sentiment Analysis")
+#     st.text("Basic app to detect the sentiment of text.")
+#     st.text("")
+#     userinput = st.text_input('Enter text below, then click the Predict button.', placeholder='Input text HERE')
+#     st.text("")
+#     predicted_sentiment = ""
+#     if st.button("Predict"):
+#         predicted_sentiment = model.predict(pd.Series(userinput))[0]
+#         if predicted_sentiment == 1:
+#             output = 'positive 👍'
+#         else:
+#             output = 'negative 👎'
+#         sentiment=f'Predicted sentiment of "{userinput}" is {output}.'
+#         st.success(sentiment)
+
+# if __name__ == "__main__":
+
 import streamlit as st
 import pandas as pd
 import joblib
@@ -6,20 +31,18 @@ from utils import preprocessor
 def run():
     model = joblib.load(open('model.joblib', 'rb'))
 
-    st.title("Sentiment Analysis")
-    st.text("Basic app to detect the sentiment of text.")
+    st.title("🌍 Language Detector")
+    st.text("Basic app to detect the language of a sentence.")
     st.text("")
-    userinput = st.text_input('Enter text below, then click the Predict button.', placeholder='Input text HERE')
+    userinput = st.text_input('Enter text below, then click the Detect button.', placeholder='Input text HERE')
     st.text("")
-    predicted_sentiment = ""
-    if st.button("Predict"):
-        predicted_sentiment = model.predict(pd.Series(userinput))[0]
-        if predicted_sentiment == 1:
-            output = 'positive 👍'
-        else:
-            output = 'negative 👎'
-        sentiment=f'Predicted sentiment of "{userinput}" is {output}.'
-        st.success(sentiment)
+    detected_language = ""
+    if st.button("Detect"):
+        cleaned_input = preprocessor(userinput)
+        detected_language = model.predict(pd.Series(cleaned_input))[0]
+        st.success(f'The detected language of "{userinput}" is **{detected_language.upper()}**.')
 
 if __name__ == "__main__":
     run()
+
+#     run()
